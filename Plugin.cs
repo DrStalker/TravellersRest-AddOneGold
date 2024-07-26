61391eadc8002161908b391f5baff42c483176a2
Doug <doug.stalker@gmail.com>
2024-07-27 00:51:26 +1000

    Initial Release 1.0.0

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace MyFirstPlugin3
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly ConfigEntry<KeyCode> _hotKey;
+        public Plugin()
+        {
+
+            _hotKey = Config.Bind("General", "HotKey", KeyCode.F10, "Press to activate mod");
+        }
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_hotKey.Value))
+            {
+                int pre = Money.ToCopper();
+                Money.APPMBNEIPOE(-10000);
+                int post = Money.ToCopper();
+                Logger.LogInfo(string.Format("Added 1Gold {0} -> {1}", pre, post));
+
+            }
+        }
+
+    }
+}
